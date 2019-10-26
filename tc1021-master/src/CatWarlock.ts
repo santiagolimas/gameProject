7f2feb43282120ca798dc401ee304a12b05b8ded
Arturo Manrique Garza <arturomgzm@hotmail.com>
2019-10-26 08:24:35 -0500

    mas animaciones

--- tc1021-master/src/CatWarlock.ts
+++ tc1021-master/src/CatWarlock.ts
@@ -15,10 +15,12 @@ class CatWarlock{
     private stance = 0;
     private sprite = new Image();
     private counter = 0;
+    private stanceChange = 0;
     private idleFrames = [[20,150],[20,275],[83,275],[147,275],[210,275],[275,275],[340,275]];
+    private chargingFrames = [[20,23],[80,23],[145,23],[205,23],[275,23],[335,23],[400,23],[465,23],[530,23],[595,23],[660,23],[725,23],[785,23]];
     position = [this.coordX,this.coordY];
     measurementsCat = [this.width,this.height];
-    private barColor = "green"
+    private barColor = "green" 
 
     constructor(coordX, coordY){
         const context = GameContext.context;
@@ -36,14 +38,48 @@ class CatWarlock{
     }
 
     public update(){
-        if(this.counter == 15){
-            this.frame++;
-            if(this.frame > 6){
+        if(this.stance == 0){
+            if(this.stanceChange <= 60){
+                this.stanceChange++;
+            }
+            else{
+                this.stanceChange = 0;
+                this.stance = 1;
                 this.frame = 0;
+                this.counter = 0;
             }
-            this.counter = 0;
+        }
+        else if(this.stance == 1){
+            if(this.stanceChange <= 91){
+                this.stanceChange++;
             }
-        this.counter++;
+            else{
+                this.stanceChange = 0;
+                this.stance = 0;
+                this.frame = 0;
+                this.counter = 0;
+            }
+        }
+        if(this.stance == 0){
+            if(this.counter == 15){
+                this.frame++;
+                if(this.frame > 6){
+                    this.frame = 0;
+                }
+                this.counter = 0;
+                }
+            this.counter++;
+        }
+        else if(this.stance == 1){
+            if(this.counter == 7){
+                this.frame++;
+                if(this.frame > 12){
+                    this.frame = 0;
+                }
+                this.counter = 0;
+                }
+            this.counter++;
+        }
         this.position[0] = this.coordX;
         this.position[1] = this.coordY;
 
@@ -55,7 +91,10 @@ class CatWarlock{
         const width = context.canvas.width
         context.save();
         context.beginPath();
-        context.drawImage(this.sprite,this.idleFrames[this.frame][0],this.idleFrames[this.frame][1],20, 35,this.coordX,this.coordY, 50, 50)
+        if(this.stance == 0 )
+            context.drawImage(this.sprite,this.idleFrames[this.frame][0],this.idleFrames[this.frame][1],20, 35,this.coordX,this.coordY, 50, 50);
+        else if(this.stance == 1)
+            context.drawImage(this.sprite,this.chargingFrames[this.frame][0],this.chargingFrames[this.frame][1],30, 35,this.coordX,this.coordY, 50, 50);
         context.closePath();
         context.restore();
 
